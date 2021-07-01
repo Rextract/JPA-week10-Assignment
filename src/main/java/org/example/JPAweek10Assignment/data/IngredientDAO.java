a7e8c84efa8d093ec308a185119e24589af19286
David Ismael A.M <https://github.com/Rextract/G36_first_lecture.git>
2021-07-01 17:41:45 +0200

    Doing the DAO classes...trying

---
+++ src/main/java/org/example/JPAweek10Assignment/data/IngredientDAO.java
@@ -0,0 +1,20 @@
+package org.example.JPAweek10Assignment.data;
+
+import org.example.JPAweek10Assignment.model.entity.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+
+public interface IngredientDAO extends JpaRepository<Ingredient, String> {
+
+    List<Ingredient> findByNameIgnoreCase(String ingredientName);
+    @Query("SELECT a FROM Ingredient a WHERE UPPER(a.ingredientName) = UPPER(:ingredientName)")
+    List<Ingredient> findByNameIgnoreCaseJPQL(
+            @Param("ingredientName") String ingredientName);
+    }
+
+
+}
