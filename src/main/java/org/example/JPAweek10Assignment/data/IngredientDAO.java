327756ae074b81424434419b457b9c7b704fc369
David Ismael A.M <https://github.com/Rextract/G36_first_lecture.git>
2021-07-02 17:51:38 +0200

    Going for the DAO second time

---
+++ src/main/java/org/example/JPAweek10Assignment/data/IngredientDAO.java
@@ -0,0 +1,17 @@
+package org.example.JPAweek10Assignment.data;
+
+import org.example.JPAweek10Assignment.model.entity.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface IngredientDAO extends JpaRepository<Ingredient, Integer> {
+
+    List<Ingredient> findByIngredientName(String ingredientName);
+    @Query("SELECT a FROM Ingredient a WHERE UPPER(a.ingredientName) = UPPER(:ingredientName)")
+    List<Ingredient> findByIngredientNameJPQL(
+            @Param("ingredientName") String ingredientName);
+
+}
