6c640879384f47cfc371c83777f0e37d505782ee
David Ismael A.M <https://github.com/Rextract/G36_first_lecture.git>
2021-07-06 14:09:08 +0200

    Going for the DAO second time

--- src/main/java/org/example/JPAweek10Assignment/data/RecipeDAO.java
+++ src/main/java/org/example/JPAweek10Assignment/data/RecipeDAO.java
@@ -3,6 +3,7 @@ package org.example.JPAweek10Assignment.data;
 import org.example.JPAweek10Assignment.model.entity.Ingredient;
 import org.example.JPAweek10Assignment.model.entity.Recipe;
 import org.example.JPAweek10Assignment.model.entity.RecipeCategory;
+import org.example.JPAweek10Assignment.model.entity.RecipeIngredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,12 +18,13 @@ public interface RecipeDAO extends JpaRepository<Recipe, Integer> {
     List<Recipe> findAllByRecipeIdJPQL(
             @Param("recipe") String recipe);
 
-   Optional<Recipe> findByRecipeNameIsContainingAndRecipeIngredients(Ingredient ingredientName);
+   Optional<Recipe> findByRecipeNameIsContainingAndRecipeIngredients(Ingredient ingredientName, RecipeIngredient recipeIngredient);
    @Query("SELECT b FROM Recipe b WHERE UPPER(b.recipeIngredients) = UPPER(:recipe)")
    Optional<Recipe> findByRecipeNameIsContainingAndRecipeIngredientsJPQL(
            @Param("recipe") String recipe);
-   
+
 
    List<Recipe> findAllByRecipeIdAndCategories(RecipeCategory category);
+
 
 }
